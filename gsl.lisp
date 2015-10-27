7aa97a3f5cc659c33fb7ad16bfef21968d248071
Bill Page <bill.page@newsynthesis.org>
2015-10-26 23:04:54 -0400

    Some miscellanious functions

--- gsl.lisp
+++ gsl.lisp
@@ -1,9 +1,8 @@
 (in-package :boot)
 (import 'gsll:gsl-lookup)
+(import (list 'gsll:nanp 'gsll:finitep 'gsl:double-float-unequal))
 (import 'gsll:integration-qng)
 ; more ...
-
-
 
 (defun |lambdaFuncallSpad| (f)
  (lambda (x) (funcall f x nil)))
@@ -14,6 +13,10 @@
 (defun |lispFromSpad| (f dom args)
  (let ((spadf (|getFunctionFromDomain| f (list dom) args)))
    (lambda (x) (spadcall x spadf))))
-   
+
+; need to avoid conflict with BOOT:INFINITYP
+(defun IsInf (a) (gsl:infinityp a))
+
+; Return multiple values as a list
 (defun integration-qng-list (f a b)
   (multiple-value-list (integration-qng f a b)))
