615104cd04684e9f50782a60f7357b82076b96be
bill.page@newsynthesis.org <bill.page@newsynthesis.org>
2015-11-01 23:55:08 -0500

    'per' function, drop gsl prefix, fix LUdecomp

--- gsl.lisp
+++ gsl.lisp
@@ -10,6 +10,10 @@
  
 (defun |mkLispFunction1| (f)
  (lambda (x) (spadcall x f)))
+(defun |mkLispFunction2| (f)
+ (lambda (x y) (spadcall x y f)))
+(defun |mkLispFunction3| (f)
+ (lambda (x y z) (spadcall x y z f)))
  
 (defun |lispFromSpad| (f dom args)
  (let ((spadf (|getFunctionFromDomain| f (list dom) args)))
@@ -18,40 +22,49 @@
 ; need to avoid conflict with BOOT:INFINITYP
 (defun IsInf (a) (gsl:infinityp a))
 
-; Return multiple values as a vector (can be interpreted as Axiom Record if number of values > 2)
-(defun integration-qng-vector (f a b)
-  (apply #'vector (multiple-value-list (integration-qng f a b))))
+;   The first parameter of each routine below, is a function 'per' written
+; in Spad which must convert the internal representation of returned
+; values to an appropriate external representation. The purpose of 'per'
+; is to minimize the number of assumptions in the Lisp code concerning the
+; representation of values.
+;   The internal representation is that obtained directly from GSLL and
+; modified if necessary to be either atomic and compatible with Spad or a
+; List of such returned values. When the output of GSL is multivalued the
+; output of 'per' is usually of type Record.
+;   The rest of the parameters must be either atomic and compatible with Spad
+; or a List of such input values. The input values are modified if necessary
+; to be compatible with GSLL.
 
-(defun lu-decomp (m r c)
+(defun integrationqng (per f a b)
+  (apply (|mkLispFunction3| per) (multiple-value-list (integration-qng (|mkLispFunction1| f) a b))))
+
+(defun ludecomp (per m nrows ncols)
   (let*
     (
-      (matrix
-        (grid:make-foreign-array 'double-float :dimensions (list r c) :initial-contents
-          (loop for row below r collecting
-            (loop for col below c collecting
-              (elt (elt m row) col)
-            )
+      (A
+        (grid:make-foreign-array 'double-float :dimensions (list nrows ncols) :initial-contents
+          (loop for row below nrows collecting
+            (loop for col below ncols collecting (elt (elt m row) col))
           )
         )
       )
     )
-    (multiple-value-bind (matrix perm) (gsl:lu-decomposition matrix)
-      (list
-        (loop for row below r collecting
-          (loop for col below c collecting
-            (grid:aref matrix row col)
-          )
+    (multiple-value-bind (A P S) (gsl:lu-decomposition A)
+      (funcall (|mkLispFunction3| per)
+        (loop for row below nrows collecting
+          (loop for col below ncols collecting (grid:aref A row col))
         )
-        (loop for row below r collecting
+        (loop for row below nrows collecting
           (grid:aref
-            (gsl:permute perm
+            (gsl:permute P
               (grid:make-foreign-array '(unsigned-byte 64) :initial-contents
-                (loop for row below r collecting (+ 1 row))
+                (loop for row below nrows collecting (+ 1 row))
               )
             )
             row
           )
         )
+        S
       )
     )
   )
